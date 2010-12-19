50bc35ac27ab682ff7ca3a38f239022ce815b01d
Ben Firshman <ben@firshman.co.uk>
2010-12-19 20:04:42 +0000

    Added distance to diff

--- needle/diff.py
+++ needle/diff.py
@@ -18,7 +18,21 @@ class ImageDiff(object):
         rmsd = 0
         for a, b in izip(a_values, b_values):
             rmsd += (a - b) ** 2
-        rmsd = math.sqrt(float(rmsd) / (self.image_a.size[0] * self.image_a.size[1] * len(self.image_a.getbands())))
+        rmsd = math.sqrt(float(rmsd) / (
+            self.image_a.size[0] * self.image_a.size[1] * len(self.image_a.getbands())
+        ))
         return rmsd / 255
+
+    def get_distance(self):
+        """
+        Returns the distance between the two images in pixels.
+        """
+        a_values = chain(*self.image_a.getdata())
+        b_values = chain(*self.image_b.getdata())
+        band_len = len(self.image_a.getbands())
+        distance = 0
+        for a, b in izip(a_values, b_values):
+            distance += abs(float(a) / band_len - float(b) / band_len) / 255
+        return distance
     
 
--- tests/test_diff.py
+++ tests/test_diff.py
@@ -4,28 +4,47 @@ from PIL import Image, ImageDraw
 import unittest2
 
 class TestImageDiff(unittest2.TestCase):
-    def test_nrmsd_all_channels(self):
-        image_a = Image.new('RGB', (100, 100), (255, 255, 255))
-        image_b = Image.new('RGB', (100, 100), (0, 0, 0))
-        diff = ImageDiff(image_a, image_b)
-        self.assertEqual(diff.get_nrmsd(), 1)
+    def get_image(self, colour):
+        return Image.new('RGB', (100, 100), colour)
 
-    def test_nrmsd_half_filled(self):
-        image_a = Image.new('RGB', (100, 100), (0, 0, 0))
-        image_b = Image.new('RGB', (100, 100), (0, 0, 0))
-        # Fill in half one rectangle
-        draw_b = ImageDraw.Draw(image_b)
-        draw_b.rectangle(
+    def get_black_image(self):
+        return self.get_image((0, 0, 0))
+
+    def get_white_image(self):
+        return self.get_image((255, 255, 255))
+
+    def get_half_filled_image(self):
+        im = self.get_black_image()
+        draw = ImageDraw.Draw(im)
+        draw.rectangle(
             ((0, 0), (49, 100)),
             fill=(255, 255, 255)
         )
-        diff = ImageDiff(image_a, image_b)
-        self.assertEqual(diff.get_nrmsd(), math.sqrt(0.5))
+        return im
+
+    def test_nrmsd_all_channels(self):
+        diff = ImageDiff(self.get_white_image(), self.get_black_image())
+        self.assertEqual(diff.get_nrmsd(), 1)
 
     def test_nrmsd_one_channel(self):
-        image_a = Image.new('RGB', (100, 100), (255, 0, 0))
-        image_b = Image.new('RGB', (100, 100), (0, 0, 0))
-        diff = ImageDiff(image_a, image_b)
-        self.assertEqual(diff.get_nrmsd(), math.sqrt(1.0/3.0))
+        diff = ImageDiff(self.get_image((255, 0, 0)), self.get_black_image())
+        self.assertEqual(diff.get_nrmsd(), math.sqrt(1.0 / 3))
+
+    def test_nrmsd_half_filled(self):
+        diff = ImageDiff(self.get_black_image(), self.get_half_filled_image())
+        self.assertEqual(diff.get_nrmsd(), math.sqrt(0.5))
+
+    def test_distance_all_channels(self):
+        diff = ImageDiff(self.get_white_image(), self.get_black_image())
+        self.assertAlmostEqual(diff.get_distance(), 100 * 100, delta=0.001)
+
+    def test_distance_one_channel(self):
+        diff = ImageDiff(self.get_image((255, 0, 0)), self.get_black_image())
+        self.assertAlmostEqual(diff.get_distance(), 10000.0 / 3, delta=0.001)
+
+    def test_distance_half_filled(self):
+        diff = ImageDiff(self.get_black_image(), self.get_half_filled_image())
+        self.assertAlmostEqual(diff.get_distance(), 10000.0 / 2, delta=0.001)
+
 
 
