381ef6aba66519701ffe8906509350476710aca9
Ben Firshman <ben@firshman.co.uk>
2010-12-19 15:06:58 +0000

    Added get_screenshot to elements

--- needle/driver.py
+++ needle/driver.py
@@ -1,9 +1,19 @@
+import base64
+from PIL import Image
 from selenium.remote.webdriver import WebDriver
 from selenium.remote.webelement import WebElement
 import urllib
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 
 class NeedleWebElement(WebElement):
     def get_dimensions(self):
+        """
+        Returns a dictionary containing, in pixels, the element's ``width`` and
+        ``height``, and it's ``left`` and ``top`` position relative to the document.
+        """
         return self._parent.execute_script("""
             var e = arguments[0];
             var dimensions = {
@@ -19,9 +29,31 @@ class NeedleWebElement(WebElement):
             return dimensions;
         """, self)
 
+    def get_screenshot(self):
+        """
+        Returns a screenshot of this element as a PIL image.
+        """
+        d = self.get_dimensions()
+        return self._parent.get_screenshot_as_image().crop((
+            d['left'],
+            d['top'],
+            d['left'] + d['width'],
+            d['top'] + d['height'],
+        ))
+
+
 class NeedleWebDriver(WebDriver):
     def get_html(self, html):
+        """
+        Load a page in the browser with the given HTML.
+        """
         self.get('data:text/html,'+urllib.quote(html))
+
+    def get_screenshot_as_image(self):
+        """
+        Returns a screenshot of the current page as a PIL image.
+        """
+        return Image.open(StringIO(base64.b64decode(self.get_screenshot_as_base64())))
     
     def create_web_element(self, *args, **kwargs):
         return NeedleWebElement(self, *args, **kwargs)
--- tests/test_driver.py
+++ tests/test_driver.py
@@ -1,11 +1,13 @@
 from needle.cases import NeedleTestCase
 
-class TestDriver(NeedleTestCase):
+class TestWebDriver(NeedleTestCase):
     def test_get_html(self):
         self.driver.get_html('<div id="test">foo</div>')
         e = self.driver.find_element_by_id('test')
         self.assertEqual(e.get_text(), 'foo')
 
+
+class TestWebElement(NeedleTestCase):
     def test_get_dimensions(self):
         self.driver.get_html('<div id="test" style="position: absolute; left: 50px; top: 100px; width: 150px; height: 200px">Test</div>')
         e = self.driver.find_element_by_id('test')
@@ -15,5 +17,15 @@ class TestDriver(NeedleTestCase):
             'width': 150,
             'height': 200,
         })
+
+    def test_get_screenshot(self):
+        self.driver.get_html('<div id="test" style="position: absolute; left: 50px; top: 100px; width: 150px; height: 200px; background-color: #FF0000"></div>')
+        e = self.driver.find_element_by_id('test')
+        im = e.get_screenshot()
+        self.assertEqual(im.size, (150, 200))
+        for pixel in im.getdata():
+            self.assertEqual(pixel, (255, 0, 0, 255))
+
+
 
 
