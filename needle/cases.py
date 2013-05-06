b4ba41d5076fd2e5a96761dab28931548f26e6b1
Chris Adams <chris@improbable.org>
2013-05-06 11:36:40 -0400

    Refactored assertScreenshot into a context manager
    This allows tests to be written in this style:
    
        def test_about(self):
            with self.compareScreenshot('body', 'about-us'):
                self.driver.get('%s/about/' % BASE_URL)
    
    along with the traditional:
    
        def test_about(self):
            self.driver.get('%s/about/' % BASE_URL)
            self.assertScreenshot('body', 'about-us'):
    
    The difference is that the former case can be significantly more
    efficient when recording baseline screenshots as it doesn't require
    loading the page simply to declare that an existing baseline will
    not be overwritten

--- needle/cases.py
+++ needle/cases.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 from __future__ import absolute_import
 
+from contextlib import contextmanager
 import os
 import sys
 
@@ -12,7 +13,7 @@ else:
 from PIL import Image
 
 from needle.diff import ImageDiff
-from needle.driver import NeedleWebDriver
+from needle.driver import NeedleWebDriver, NeedleWebElement
 
 
 def _object_filename(obj):
@@ -61,36 +62,54 @@ class NeedleTestCase(TestCase):
         # TODO: Should baseline be a top-level peer to output_directory?
         self.baseline_directory = os.environ.get('NEEDLE_BASELINE_DIR', os.path.realpath(os.path.join(os.getcwd(), 'screenshots', 'baseline')))
 
-    def assertScreenshot(self, element, name, threshold=0.1):
+    def assertScreenshot(self, element_or_selector, filename, threshold=0.1):
+        """assert-style variant of compareScreenshot context manager
+
+        compareScreenshot() can be considerably more efficient for recording baselines by avoiding the need
+        to load pages before checking whether we're actually going to save them. This function allows you
+        to continue using normal unittest-style assertions if you don't need the efficiency benefits
+        """
+
+        with self.compareScreenshot(element_or_selector, filename, threshold=threshold):
+            pass
+
+    @contextmanager
+    def compareScreenshot(self, element_or_selector, filename, threshold=0.1):
         """
         Assert that a screenshot of an element is the same as a screenshot on disk,
         within a given threshold.
 
-        :param element: Either a CSS selector as a string or a
-                        :py:class:`~needle.driver.NeedleWebElement` object that
-                        represents the element to capture.
-        :param name: A name for the screenshot, which will be appended with
-                     ``.png``.
-        :param threshold: The threshold for triggering a test failure.
+        :param element_or_selector:
+            Either a CSS selector as a string or a
+            :py:class:`~needle.driver.NeedleWebElement` object that represents
+            the element to capture.
+        :param name:
+            A name for the screenshot, which will be appended with ``.png``.
+        :param threshold:
+            The threshold for triggering a test failure.
         """
-        if isinstance(element, basestring):
-            element = self.driver.find_element_by_css_selector(element)
 
-        if not isinstance(name, basestring):
-            raise NotImplementedError  # This needs to be reconciled below without worsening code-smell
-            # names can be filehandles for testing. This sucks - we
-            # should write out files to their correct location
-            filename = name
+        if not isinstance(filename, basestring):
+            raise NotImplementedError
 
-        baseline_file = os.path.join(self.baseline_directory, '%s.png' % name)
-        output_file = os.path.join(self.output_directory, '%s.png' % name)
+        baseline_file = os.path.join(self.baseline_directory, '%s.png' % filename)
+        output_file = os.path.join(self.output_directory, '%s.png' % filename)
+
+        if self.capture and os.path.exists(baseline_file):
+            self.skipTest('Not capturing %s, image already exists. If you '
+                          'want to capture this element again, delete %s'
+                          % (filename, baseline_file))
+
+        yield
+
+        if not isinstance(element_or_selector, NeedleWebElement):
+            element = self.driver.find_element_by_css_selector(element_or_selector)
+        else:
+            element = element_or_selector
 
         if self.capture:
-            if os.path.exists(baseline_file):
-                self.skipTest('Not capturing %s, image already exists. If you '
-                              'want to capture this element again, delete %s'
-                              % (name, baseline_file))
             element.get_screenshot().save(baseline_file)
+            return
         else:
             screenshot = element.get_screenshot()
             screenshot.save(output_file)
@@ -102,4 +121,4 @@ class NeedleTestCase(TestCase):
             if distance > threshold:
                 raise AssertionError("The saved screenshot for '%s' did not match "
                                      "the screenshot captured (by a distance of %.2f)"
-                                     % (name, distance))
+                                     % (filename, distance))
