422469be6f8ed6b947edbdf54feaf7cf14445dc8
Ben Firshman <ben@firshman.co.uk>
2013-10-24 21:32:50 +0100

    Set correct version in needle.__version__

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+import re
+import os
+import codecs
+
+
+# Borrowed from
+# https://github.com/jezdez/django_compressor/blob/develop/setup.py
+def read(*parts):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='needle',
-    version='0.0.2',
+    version=find_version("needle", "__init__.py"),
     description='Automated testing for your CSS.',
     author='Ben Firshman',
     author_email='ben@firshman.co.uk',
