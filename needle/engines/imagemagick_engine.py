e29778cdd9ed5cbc711a1deca0f5e12dac5bbb30
Julien Phalip <jphalip@gmail.com>
2015-09-18 21:31:59 -0700

    Removed the use of the ``cmd`` command in the ImageMagick engine as we can't guarantee that all users will have it available.

--- needle/engines/imagemagick_engine.py
+++ needle/engines/imagemagick_engine.py
@@ -5,23 +5,12 @@ from needle.engines.base import EngineBase
 
 
 class Engine(EngineBase):
-    cmp_path = "cmp"
-    cmp_command = "{cmp} {baseline} {new}"
     compare_path = "compare"
     compare_command = ("{compare} -metric RMSE -subimage-search {baseline} "
                        "{new} {diff}")
 
     def assertSameFiles(self, output_file, baseline_file, threshold=0):
         diff_file = output_file.replace('.png', '.diff.png')
-
-        cmp_cmd = self.cmp_command.format(cmp=self.cmp_path,
-                                          baseline=baseline_file,
-                                          new=output_file)
-        if subprocess.call(cmp_cmd, shell=True) == 0:
-            os.remove(output_file)
-            # remove a possible earlier diff file
-            os.remove(diff_file)
-            return
 
         compare_cmd = self.compare_command.format(
             compare=self.compare_path,
@@ -33,20 +22,9 @@ class Engine(EngineBase):
                                    stderr=subprocess.PIPE)
         compare_stdout, compare_stderr = process.communicate()
 
-        if process.returncode != 0:
-            raise RuntimeError("{compare} returned a non-zero exit status.\n"
-                               "{cmd}\n"
-                               "{stdout}{stderr}"
-                               .format(compare=self.compare_path,
-                                       cmd=compare_cmd,
-                                       stdout=compare_stdout,
-                                       stderr=compare_stderr))
-
         difference = float(compare_stderr.split()[1][1:-1])
-
         if difference <= threshold:
             os.remove(diff_file)
-            os.remove(output_file)
             return
 
         raise AssertionError("The new screenshot '{new}' did not match "
