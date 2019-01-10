b88aaa3bee990eb32851930093be29a969590bff
Nazar Goshko <n.goshko@softerra.com>
2019-01-10 20:21:18 +0200

    Fix problem with space in paths

--- needle/engines/imagemagick_engine.py
+++ needle/engines/imagemagick_engine.py
@@ -5,23 +5,23 @@ from needle.engines.base import EngineBase
 
 
 class Engine(EngineBase):
+    
     compare_path = "compare"
-    compare_command = ("{compare} -metric RMSE -subimage-search -dissimilarity-threshold 1.0 {baseline} "
-                       "{new} {diff}")
 
     def assertSameFiles(self, output_file, baseline_file, threshold=0):
         diff_file = output_file.replace('.png', '.diff.png')
 
-        compare_cmd = self.compare_command.format(
-            compare=self.compare_path,
-            baseline=baseline_file,
-            new=output_file,
-            diff=diff_file)
-        process = subprocess.Popen(compare_cmd, shell=True,
+        compare_command = [self.compare_path,
+                           "-metric","RMSE",
+                           "-subimage-search",
+                           "-dissimilarity-threshold","1.0",
+                           baseline_file, output_file, diff_file]
+
+        process = subprocess.Popen(compare_command, shell=True,
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
         compare_stdout, compare_stderr = process.communicate()
-
+        print(compare_stderr)
         difference = float(compare_stderr.split()[1][1:-1])
         if difference <= threshold:
             os.remove(diff_file)
